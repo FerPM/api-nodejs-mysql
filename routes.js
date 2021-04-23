d01fbd1b50a3521b072dc869988b50d059d7cc77
FerPM <fer.a.g10b@gmail.com>
2021-04-23 03:05:38 -0500

    method put

--- routes.js
+++ routes.js
@@ -24,4 +24,27 @@ routes.post('/', (req, res)=>{
     })
 })
 
+routes.delete('/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('DELETE FROM books WHERE id = ?', [req.params.id], (err,rows)=>{
+           if(err) return res.send(err)
+
+           res.send('book excluded!')
+        })
+    })
+})
+
+
+routes.put('/:id', (req, res)=>{
+    req.getConnection((err, conn)=>{
+        if(err) return res.send(err)
+        conn.query('UPDATE books set ? WHERE id = ?', [req.body, req.params.id], (err,rows)=>{
+           if(err) return res.send(err)
+
+           res.send('book update!')
+        })
+    })
+})
+
 module.exports = routes
